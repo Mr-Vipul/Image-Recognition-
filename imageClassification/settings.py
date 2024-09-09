244be6c2cce9026e2d1140c9bd9013137fe51c1a
Vipul <goyalvipul2002@gmail.com>
2024-09-09 21:12:22 +0530

    Html update

--- imageClassification/settings.py
+++ imageClassification/settings.py
@@ -18,7 +18,7 @@ from tensorflow.python.keras.backend import set_session
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
